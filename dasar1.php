1cbb711517e921e213c1be3fbec797846efc41fb
xwahuy04 <wahyuhavidbaihaqi@gmail.com>
2024-11-05 21:15:37 +0700

    first commit

---
+++ dasar1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+class Fruit {
+  // Properties
+  public $name;
+  public $color;
+
+  // Methods
+//   function set_name($name) {
+
+//     return $this->name = $name;
+//   }
+
+//   function set_color($color) {
+//     return $this->color = $color;
+//   }
+ 
+}
+
+//declare
+// $object = new class();
+
+$apple = new Fruit();
+// object
+// $name_object->property =  "";
+// $apple->name = "Apple";
+
+
+$banana = new Fruit();
+// object
+// $name_object->property =  "";
+// $banana->name = "Banana";
+
+// echo "Name : ". $apple->set_name("apple");
+// echo "<br>";
+// echo $apple->set_color("red");
+// echo "<br>";
+// echo $banana->set_name("apple");
+
+$apple->name;
+?>
+ 
+</body>
+</html>
